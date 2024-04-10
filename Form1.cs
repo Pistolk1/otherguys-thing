f3916d1eab567e5e7ff339eb866960da518e012f
qfoxb <42038239+qfoxb@users.noreply.github.com>
2024-04-10 08:51:23 -0500

    single place version downloading
    the issue i was experiencing was due to compression

--- Form1.cs
+++ Form1.cs
@@ -1,3 +1,6 @@
+using System.IO.Compression;
+using System.Net;
+using System.Net.Http.Headers;
 using System.Text;
 
 namespace Roblox_Place_Downloader
@@ -22,12 +25,50 @@ namespace Roblox_Place_Downloader
             System.Diagnostics.Process.Start(new System.Diagnostics.ProcessStartInfo { FileName = target, UseShellExecute = true });
         }
 
-        private async void button1_Click(object sender, EventArgs e) // Download Single Place Version
+        private void button1_Click(object sender, EventArgs e) // Download Single Place Version
         {
             long placeId = long.Parse(textBox1.Text);
             long versionId = long.Parse(textBox2.Text);
             string url = "https://assetdelivery.roblox.com/v1/asset/?id=" + placeId + "&versionId=" + versionId;
-            
+
+            using (WebClient client = new WebClient())
+            {
+                client.Headers[HttpRequestHeader.AcceptEncoding] = "gzip";
+                byte[] compressedBytes = client.DownloadData(url);
+
+                using (GZipStream decompressionStream = new GZipStream(new MemoryStream(compressedBytes), CompressionMode.Decompress))
+                {
+                    const int blockSize = 1024;
+                    byte[] buffer = new byte[blockSize];
+                    using (MemoryStream memory = new MemoryStream())
+                    {
+                        int count = 0;
+                        do
+                        {
+                            count = decompressionStream.Read(buffer, 0, blockSize);
+                            if (count > 0)
+                            {
+                                memory.Write(buffer, 0, count);
+                            }
+                        }
+                        while (count > 0);
+
+                        byte[] decompressedBytes = memory.ToArray();
+
+                        SaveFileDialog saveSingleFile = new SaveFileDialog();
+                        saveSingleFile.Filter = "Roblox Place Files (*.rbxl)|*.rbxl";
+                        saveSingleFile.Title = "Save Roblox Place File";
+
+                        if (saveSingleFile.ShowDialog() == DialogResult.OK)
+                        {
+                            string filePath = saveSingleFile.FileName;
+                            File.WriteAllBytes(filePath, decompressedBytes);
+                        }
+                    }
+                }
+            }
+
+
         }
     }
 }
